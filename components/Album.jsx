84e2194efcf7895f093cc28bd238fd6480109fa4
iortega-dev <iortegamancera@gmail.com>
2020-06-16 13:26:11 +0200

    [ADD] Implemented full SSR data load

--- components/Album.jsx
+++ components/Album.jsx
@@ -11,23 +11,24 @@ import {
     Row
 } from 'reactstrap';
 
-const Album = ({ album }) => {
+const Album = ({ allCharactersData, error }) => {
     return (
         <div className="album py-5 bg-light">
             <Container>
                 <Row>
-                    {album.map((item, key) => {
+                    {error && <div>There was an error.</div>}
+                    {!error && allCharactersData && allCharactersData.data.map((item, key) => {
                         return (
-                            <Col md="4" key={key}>
+                            <Col md="3" key={key}>
                                 <Card className="mb-4 box-shadow">
                                     <CardImg
                                         top
                                         width="100%"
-                                        src={item.src}
-                                        alt={item.altText}
+                                        src={item.image}
+                                        alt={item.species}
                                     />
                                     <CardBody>
-                                        <CardText>{item.description}</CardText>
+                                        <CardText>{item.name}</CardText>
                                     </CardBody>
                                 </Card>
                             </Col>

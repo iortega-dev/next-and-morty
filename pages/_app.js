cd303d979297705063d24dd77a9988a2608bb5dc
iortega-dev <iortegamancera@gmail.com>
2020-06-16 11:04:34 +0200

    [ADD] Added index content and app styles

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}

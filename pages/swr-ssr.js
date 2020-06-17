7fee06f9704a6204075dae12d4b7150154298882
iortega-dev <iortegamancera@gmail.com>
2020-06-17 12:31:48 +0200

    [ADD] Added SSR with Loadmore

--- pages/swr-ssr.js
+++ pages/swr-ssr.js
@@ -1,22 +1,28 @@
-import Head from 'next/head'
 import { useState } from 'react';
+import useSWR from 'swr';
+
 
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
 
 import useRequest from '../libs/useRequest';
+import fetch from '../libs/fetch'
 
-export default function SWR() {
+export default function SWR(props) {
+  const initialData = props.data
   const [collapsed, setCollapsed] = useState(true)
 
   const toggleNavbar = () => {
     setCollapsed(!collapsed)
   }
 
-  const data = useRequest({
-    url: '/api/data'
-  })
+  const { data, mutate } = useSWR('/api/data', fetch, { initialData })
+
+  const loadMore = async () => {
+    const newData = await fetch('https://rickandmortyapi.com/api/character/?page=2')
+    mutate(newData.results, false)
+  }
 
   return (
     <div>
@@ -24,9 +30,14 @@ export default function SWR() {
         collapsed={collapsed}
         toggleNavbar={toggleNavbar}
       />
-      <Main allCharactersData={data} />
+      <Main allCharactersData={{data}} onLoadMoreSSR={loadMore} />
       <Footer />
     </div>
   )
 }
 
+/* https://github.com/vercel/swr#ssr-with-nextjs */
+export async function getServerSideProps() {
+  const data = await fetch('https://rickandmortyapi.com/api/character/')
+  return { props: { data: data.results } }
+}
